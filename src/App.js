3273d98acf87bf5b15769717f7d1e50eecc8051d
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-14 10:20:55 -0400

    [ADD] Add principal content with styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
+import styled from '@emotion/styled';
+import image from './cryptocurrency.png'
+
+const Container = styled.div`
+  display: grid;
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width: 992px) {
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #fff;
+  text-align: left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+  letter-spacing: 1.25px;
+
+  &::after {
+    content: '';
+    width: 162px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+  }
+`;
 
 function App() {
   return (
-    <h1>Crypto Quoter</h1>
+    <Container>
+      <div>
+        <Image
+          src={image}
+          alt='crypto-image'
+        />
+      </div>
+      <div>
+        <Heading>Trade Cryptocurrencies Instantly</Heading>
+      </div>
+    </Container>
   );
 }
 
