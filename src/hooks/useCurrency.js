bf550d6a4fe9cd87b0eb800dfe0653c8395f18c8
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-14 22:58:15 -0400

    [FEAT] Read what user selects from custom hook

--- src/hooks/useCurrency.js
+++ src/hooks/useCurrency.js
@@ -7,7 +7,10 @@ const useCurrency = (label, initialState, options) => {
     const Select = () => (
         <Fragment>
             <label>{label}</label>
-            <select>
+            <select
+                onChange={e => updateState(e.target.value)}
+                value={state}
+            >
                 <option value=''>--Select--</option>
                 {options.map (option => (
                     <option key={option.code} value={option.code}>{option.name}</option>
