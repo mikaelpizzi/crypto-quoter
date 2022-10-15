c2b67f836c1a5e04608bc861a1b18abbeb872d22
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-15 16:22:10 -0400

    [ADD] Add styles to results

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,15 +1,38 @@
+import styled from "@emotion/styled";
+
+const ResultDiv = styled.div`
+    color: #fff;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Info = styled.p`
+    font-size: 18px;
+
+    span {
+        font-weight: bold;
+    }
+`;
+
+const Price = styled.p`
+    font-size: 30px;
+
+    span {
+        font-weight: bold;
+    }
+`;
+
 const Quote = ({ result }) => {
     if (Object.keys(result).length === 0) return null;
 
     console.log(result);
 
     return ( 
-        <div>
-            <p>The price is: <span>{result.PRICE}</span></p>
-            <p>The market capitalization is: <span>{result.MKTCAP}</span></p>
-            <p>Last 24h: <span>{result.CHANGEPCT24HOUR}%</span> &nbsp; / &nbsp;<span>{result.CHANGE24HOUR}</span></p>
-            <p>Last update: <span>{result.LASTUPDATE}</span></p>
-        </div>
+        <ResultDiv>
+            <Price>The price is: <span>{result.PRICE}</span></Price>
+            <Info>The market capitalization is: <span>{result.MKTCAP}</span></Info>
+            <Info>Last 24h: <span>{result.CHANGEPCT24HOUR}%</span> &nbsp; / &nbsp; <span>{result.CHANGE24HOUR}</span></Info>
+            <Info>Last update: <span>{result.LASTUPDATE}</span></Info>
+        </ResultDiv>
      );
 }
  
