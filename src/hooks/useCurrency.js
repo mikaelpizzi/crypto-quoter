70d55f903d8eac1000b2e213cd5b38075324ec9c
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-14 23:19:41 -0400

    [ADD] Add styles to custom hook

--- src/hooks/useCurrency.js
+++ src/hooks/useCurrency.js
@@ -1,4 +1,24 @@
 import { Fragment, useState } from "react";
+import styled from "@emotion/styled";
+
+const Label = styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 2.4rem;
+    margin-top: 2rem;
+    display: block;
+`;
+
+const Selection = styled.select`
+    width: 100%;
+    display: block;
+    padding: 1rem;
+    -webkit-appearance: none;
+    border-radius: 10px;
+    border: none;
+`;
 
 const useCurrency = (label, initialState, options) => {
     // Custom hook state
@@ -6,8 +26,8 @@ const useCurrency = (label, initialState, options) => {
 
     const Select = () => (
         <Fragment>
-            <label>{label}</label>
-            <select
+            <Label>{label}</Label>
+            <Selection
                 onChange={e => updateState(e.target.value)}
                 value={state}
             >
@@ -15,7 +35,7 @@ const useCurrency = (label, initialState, options) => {
                 {options.map (option => (
                     <option key={option.code} value={option.code}>{option.name}</option>
                 ))}
-            </select>
+            </Selection>
         </Fragment>
     );
 
